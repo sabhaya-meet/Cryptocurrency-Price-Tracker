dc96951df971ececa3259f8a1c08863b6146d67e
sabhaya meet <sabhayameet76980@gmail.com>
2025-05-21 17:49:45 +0530

    added landing page and animation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./components/Common/Footer/Footer";
 import Header from "./components/Common/Header/Header";
+import MainComponent from "./components/LandingPage/MainComponent";
 
 function App() {
   return (
     <div>
       <Header />
+      <MainComponent />
       {/* <Footer /> */}
     </div>
   );
